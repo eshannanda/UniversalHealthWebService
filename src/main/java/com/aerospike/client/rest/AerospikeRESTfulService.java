19768d4dbd4db82ffa72564d483b5f285da6d096
Nanda <Eshan.Nanda@dell.com>
2020-05-07 01:43:33 +0530

    Revert "Merge branch 'master' of https://github.com/eshannanda/UniversalHealthWebService"
    This reverts commit 5193fc04ff9e599483879f04791ef40d1fc03f5b, reversing
    changes made to d3e9233c2d1262e4ee267d538996fbe895c2dddb.

--- src/main/java/com/aerospike/client/rest/AerospikeRESTfulService.java
+++ src/main/java/com/aerospike/client/rest/AerospikeRESTfulService.java
@@ -4,9 +4,6 @@
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
  *
-
-
-Hello
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at http://www.apache.org/licenses/LICENSE-2.0
